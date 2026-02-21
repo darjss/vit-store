5fffd7695d01482b823878efca6a981cb719ae25
darjs <darjs@users.noreply.github.com>
2026-02-21 17:36:08 +0800

    move session domain config to request time

--- packages/api/src/lib/session/store.ts
+++ packages/api/src/lib/session/store.ts
@@ -4,7 +4,6 @@ export const storeSessionManager = createCustomerSessionManager({
 	kvSessionPrefix: "store_session",
 	kvUserSessionPrefix: "store_user_sessions",
 	cookieName: "store_session",
-	domainEnvVar: "STORE_DOMAIN",
 	sessionDurationMs: 1000 * 60 * 60 * 24 * 7, // 7 days
 	renewalThresholdMs: 1000 * 60 * 30, // 30 minutes
 });
