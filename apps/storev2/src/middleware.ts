0c1f04f40c362d91294e6781632d0c69dd0640ce
darjss <darjsavid@gmail.com>
2026-05-21 22:21:22 +0800

    add ci deploy

--- apps/storev2/src/middleware.ts
+++ apps/storev2/src/middleware.ts
@@ -8,18 +8,5 @@ export const onRequest = defineMiddleware((context, next) => {
 		return context.redirect("/robots.txt", 301);
 	}
 
-	const isAssetOrFile = /\/[^/]+\.[^/]+$/.test(url.pathname);
-	const isBackendOrApiRoute =
-		url.pathname.startsWith("/api/") || url.pathname.startsWith("/trpc/");
-	if (
-		url.pathname !== "/" &&
-		!url.pathname.endsWith("/") &&
-		!isAssetOrFile &&
-		!isBackendOrApiRoute
-	) {
-		url.pathname = `${url.pathname}/`;
-		return context.redirect(`${url.pathname}${url.search}`, 301);
-	}
-
 	return next();
 });
